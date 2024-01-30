d2126f8860a452dafd2d51df2b03b2c740f766dd
MRAUAA <aaronureta9@gmail.com>
2024-01-30 16:06:20 -0500

    Cambios necesarios
    después de hacer 2 trabajos y ver que no habia solución he decidido juntar ciertos codigos y hacerlo algo simple juntando html y css, con la base principal python y mongo db

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask, render_template, request
-from pymongo import MongoClient
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask_pymongo import PyMongo
+import bcrypt
 
 app = Flask(__name__)
 
-# Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017')
-db = client['mydatabase']
-users_collection = db['users']
+app.config['MONGO_DBNAME'] = 'mongologinexample'
+app.config['MONGO_URI'] = 'mongodb+srv://mrauaa:<aaronymilo13>@cluster0.lufmv6r.mongodb.net/?retryWrites=true&w=majority'
 
-@app.route('/', methods=['GET', 'POST'])
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
 def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if users_collection.find_one({'username': username, 'password': password}):
-            return 'Login successful'
-        else:
-            return 'Invalid username or password'
-    else:
-        return render_template('login.html')
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(debug=True)
-    
